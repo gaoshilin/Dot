1f330f60cbbeeb9024076914f4f34f7f2072c790
gaoshilin <375089259@qq.com>
2016-06-14 15:23:57 +0800

    添加 WeightCalculatorBase 类

---
+++ Dot/LoadBalance/Weight/WeightCalculatorBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dot.LoadBalance.Weight
+{
+    public abstract class WeightCalculatorBase<T> : IWeightCalculator<T>
+    {
+        public int Calculate(T item)
+        {
+            var weight = this.DoCalculate(item);
+            return weight < 1 ? 1 : weight;
+        }
+        public virtual List<int> Calculate(List<T> items)
+        {
+            return items.Select(item => this.Calculate(item)).ToList();
+        }
+        protected abstract int DoCalculate(T item);
+    }
+}
