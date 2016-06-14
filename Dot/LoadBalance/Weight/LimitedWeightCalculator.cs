916ed73f501a569bf6f79e8f992a6d8f3a8047ed
gaoshilin <375089259@qq.com>
2016-06-14 16:25:15 +0800

    优化 LimitedWeightCalculator 算法

--- Dot/LoadBalance/Weight/LimitedWeightCalculator.cs
+++ Dot/LoadBalance/Weight/LimitedWeightCalculator.cs
@@ -21,15 +21,10 @@ namespace Dot.LoadBalance.Weight
             var weightPrecents = originalWeights.Select(weight => (double)weight / totalWeight).ToList();
 
             totalWeight = Math.Min(_limit, totalWeight);
-            //var weights = weightPrecents.Select(precent =>
-            //{
-            //    var weight = precent * totalWeight;
-            //    return Math.Max(1, (int)weight);
-            //}).ToList();
-
             var weights = weightPrecents.Select(precent => Math.Max(1, (int)(precent * totalWeight))).ToList();
             var gcd = weights.GetGreatestCommonDivisor();
-            weights = weights.Select(weight => weight / gcd).ToList();
+            if (gcd != 1)
+                weights = weights.Select(weight => weight / gcd).ToList();
 
             return weights;
         }
