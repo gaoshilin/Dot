0a7c3a735014d2875d3b6953d523414711275fc0
gaoshilin <375089259@qq.com>
2016-06-20 11:02:01 +0800

    为 ICompare<T> 接口新增扩展方法

---
+++ Dot/Extension/IComparableExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Dot.Extension
+{
+    public static class IComparableExtension
+    {
+        public static bool GreaterThan<T>(this T param, T comparand)
+            where T : IComparable<T>
+        {
+            return param.CompareTo(comparand) == 1;
+        }
+
+        public static bool EqualThan<T>(this T param, T comparand)
+            where T : IComparable<T>
+        {
+            return param.CompareTo(comparand) == 0;
+        }
+
+        public static bool SmallerThan<T>(this T param, T comparand)
+            where T : IComparable<T>
+        {
+            return param.CompareTo(comparand) == -1;
+        }
+
+        public static bool GreaterOrEqualThan<T>(this T param, T comparand)
+            where T : IComparable<T>
+        {
+            return param.CompareTo(comparand) >= 0;
+        }
+
+        public static bool SmallerOrEqualThan<T>(this T param, T comparand)
+            where T : IComparable<T>
+        {
+            return param.CompareTo(comparand) <= 0;
+        }
+    }
+}
