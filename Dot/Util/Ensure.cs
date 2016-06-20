29ff11e84ee9fac1038f13d62e4559491f5ba704
gaoshilin <375089259@qq.com>
2016-06-20 11:01:13 +0800

    优化代码

--- Dot/Util/Ensure.cs
+++ Dot/Util/Ensure.cs
@@ -26,28 +26,44 @@ namespace Dot.Util
                 throw new ArgumentNullException(paramName, paramName + " can not be null or empty.");
         }
 
-        public static void Greater(int param, int comparand, string paramName)
+        public static void Greater<T>(T param, T comparand, string paramName)
+            where T : IComparable<T>
         {
-            if (param <= comparand)
+            if (!param.GreaterThan(comparand))
                 throw new ArgumentException(paramName, "{0}[{1}] must greater than {2}".FormatWith(paramName, param, comparand));
         }
 
-        public static void GreaterOrEqual(int param, int comparand, string paramName)
+        public static void GreaterOrEqual<T>(T param, T comparand, string paramName)
+            where T : IComparable<T>
         {
-            if (param < comparand)
+            if (!param.GreaterOrEqualThan(comparand))
                 throw new ArgumentException(paramName, "{0}[{1}] must greater or equal than {2}".FormatWith(paramName, param, comparand));
         }
 
-        public static void Equal(Type expected, Type actual, string paramName)
+        public static void Smaller<T>(T param, T comparand, string paramName)
+            where T : IComparable<T>
         {
-            if (expected != actual)
+            if (!param.SmallerThan(comparand))
+                throw new ArgumentException(paramName, "{0}[{1}] must smaller than {2}".FormatWith(paramName, param, comparand));
+        }
+
+        public static void SmallerOrEqual<T>(T param, T comparand, string paramName)
+            where T : IComparable<T>
+        {
+            if (!param.SmallerOrEqualThan(comparand))
+                throw new ArgumentException(paramName, "{0}[{1}] must smaller or equal than {2}".FormatWith(paramName, param, comparand));
+        }
+
+        public static void EqualThan<T>(T expected, T actual, string paramName)
+        {
+            if (!object.Equals(expected, actual))
                 throw new ArgumentException(paramName, "{0} must be type of {1}, current type is {2}".FormatWith(paramName, expected, actual));
         }
 
         public static void True(bool isTrue, string paramName, string message)
         {
             if (!isTrue)
-                throw new ArgumentException(paramName, message);
+                throw new ArgumentException(paramName, message);                                                                                                                                                                                                                                                                                                                                                                                                                          
         }
 
         public static void True(bool isTrue, string message)
